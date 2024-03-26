5f6adb9543cebe6cea4faf516c970bd9593c214e
RBxRobson <robsonnbarbosa0@gmail.com>
2024-03-26 16:31:26 -0300

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Counter } from './components/Counter'
+
+function App() {
+  return (
+    <div>
+      <h1>Vite + React</h1>
+      <div>
+        <Counter />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  define: {
+    'process.env': process.env,
+    global: 'window'
+  },
+  build: {
+    commonjsOptions: { include: [] }
+  },
+  optimizeDeps: {
+    disabled: false
+  }
+})
